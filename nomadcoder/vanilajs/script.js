be3b42b35808048ed54d914aea9e8fb17b751692
Gwanwoo Shin <tlsrhksdn23@gmail.com>
2024-03-23 05:16:01 +0000

    freecodecamp start

---
+++ nomadcoder/vanilajs/script.js
@@ -0,0 +1,23 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+
+const checkRequired() {
+
+}
+
+const checkLength() {
+
+
+}
+
+const checkEmail() {
+
+}
+
+const checkPasswordsMatch() {
+
+}
---
+++ vanilawebprojects/FormValidation/script.js
@@ -0,0 +1,22 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+function checkRequired() {
+  
+}
+
+function checkLength() {
+
+}
+
+function checkEmail() {
+  
+}
+
+function checkPasswordsMatch() {
+
+}
+
